f0f3ee174cf82955d1f4f983e7041d47c3a17581
Michael Hueschen <michaelhueschen@gmail.com>
2013-04-09 08:42:08 -0500

    .gitignore now working

--- 322-interps/run.rkt
+++
@@ -1,5 +0,0 @@
-#lang racket/base 
-(require "1-interp.ss")
-(L1-interp/file "fib-rec.L1")
-(L2-interp/file "fib-rec.L2")
-
--- 322-interps/tests/mhuesch/1-test/7.res
+++
@@ -1,1 +0,0 @@
-attempted to use position 51 in an array that only has 50 positions
