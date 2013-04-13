4813564fdedb5aec2bc01a1109ef6f6d6bd82d96
Michael Hueschen <michaelhueschen@gmail.com>
2013-04-12 22:16:14 -0500

    Identity function for result file

---
+++ src/Spill/Id.hs
@@ -0,0 +1,21 @@
+module Main where
+
+import Control.Monad
+import System.Exit (exitFailure)
+import System.Environment
+import Text.ParserCombinators.Parsec hiding (State)
+
+import L2.Grammar
+import L2.Parser
+import L2.Display
+
+main :: IO ()
+main = do
+    args <- getArgs
+    when (length args == 0) $ do
+        putStrLn "usage: filename"
+        exitFailure
+    result <- parseFromFile parseMain (args !! 0)
+    case result of
+        Left err -> putStrLn . show $ err
+        Right sp -> putStrLn . displayInstrList $ sp
