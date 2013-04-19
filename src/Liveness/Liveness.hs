811a6b9ae5ffe43f249887ccec23ee6eef5312be
Michael Hueschen <michaelhueschen@gmail.com>
2013-04-19 17:08:45 -0500

    kill function and tests

--- src/Liveness/Liveness.hs
+++ src/Liveness/Liveness.hs
@@ -62,37 +62,55 @@ calleeSaveRegX :: [L2X]
 calleeSaveRegX = map L2Xreg [L2EDI, L2ESI]
 
 x86CallerSaveRegX :: [L2X]
-x86CallerSaveRegX = map L2Xreg [L2EAX, L2EBX, L2ECX]
+x86CallerSaveRegX = map L2Xreg [L2EAX, L2ECX, L2EDX]
+
 
 gen :: L2Instruction -> S.Set L2X
 gen (L2Assign _ (L2SX x)) = liveSet [x]
 gen (L2ReadMem _ x _) = liveSet [x]
-gen (L2Update x1 _ s) = case s of
-    (L2SX x2) -> liveSet [x1,x2]
-    _ -> liveSet [x1]
-gen (L2Arith x1 _ (L2TX x2)) = liveSet [x1,x2]
 gen (L2ShiftSX x1 _ x2) = liveSet [x1,x2]
 gen (L2ShiftNum x _ _) = liveSet [x]
-gen (L2SaveCmp _ (L2TX x1) _ (L2TX x2)) = liveSet [x1,x2]
-gen (L2Cjump (L2TX x1) _ (L2TX x2) _ _) = liveSet [x1,x2]
+gen (L2SaveCmp _ t1 _ t2) = extractXs t1 t2
+gen (L2Cjump t1 _ t2 _ _) = extractXs t1 t2
 gen (L2Call (L2UX x)) = liveSet $ [x] ++ argRegX
 gen (L2TailCall (L2UX x)) = liveSet $ [x] ++ argRegX ++ calleeSaveRegX
 gen (L2Return) = liveSet $ resultRegX ++ calleeSaveRegX
 gen (L2Print (L2TX x)) = liveSet [x]
-gen (L2Allocate t1 t2) = case (t1,t2) of
-    (L2TX x1, L2TX x2) -> liveSet [x1,x2]
-    (L2TX x, L2Tnum _) -> liveSet [x]
-    (L2Tnum _, L2TX x) -> liveSet [x]
-    otherwise -> liveSet []
-gen (L2ArrayError t1 t2) = case (t1,t2) of
-    (L2TX x1, L2TX x2) -> liveSet [x1,x2]
-    (L2TX x, L2Tnum _) -> liveSet [x]
-    (L2Tnum _, L2TX x) -> liveSet [x]
-    otherwise -> liveSet []
+gen (L2Allocate t1 t2) = extractXs t1 t2
+gen (L2ArrayError t1 t2) = extractXs t1 t2
+
+gen (L2Update x1 _ s) = case s of
+    (L2SX x2) -> liveSet [x1,x2]
+    _ -> liveSet [x1]
+
+gen (L2Arith x1 _ t) = case t of
+    (L2TX x2) -> liveSet [x1,x2]
+    _ -> liveSet []
+
 gen _ = liveSet []
 
 
+extractXs :: L2T -> L2T -> S.Set L2X
+extractXs t1 t2 = case (t1,t2) of
+    (L2TX x1, L2TX x2) -> liveSet [x1,x2]
+    (L2TX x, L2Tnum _) -> liveSet [x]
+    (L2Tnum _, L2TX x) -> liveSet [x]
+    _ -> liveSet []
 
+
+kill :: L2Instruction -> S.Set L2X
+kill (L2Assign x _) = liveSet [x]
+kill (L2ReadMem x _ _) = liveSet [x]
+kill (L2Update _ _ _) = liveSet []
+kill (L2Arith x _ _) = liveSet [x]
+kill (L2ShiftSX x _ _) = liveSet [x]
+kill (L2ShiftNum x _ _) = liveSet [x]
+kill (L2SaveCmp x _ _ _) = liveSet [x]
+kill (L2Call _) = liveSet $ callerSaveRegX ++ resultRegX
+kill (L2Print _) = liveSet x86CallerSaveRegX
+kill (L2Allocate _ _) = liveSet x86CallerSaveRegX
+kill (L2ArrayError _ _) = liveSet x86CallerSaveRegX
+kill _ = liveSet []
 
 
 
--- test/Liveness/Tests.hs
+++ test/Liveness/Tests.hs
@@ -10,35 +10,67 @@ import L2.Display
 import Liveness.Liveness
 
 
-main = runTestTT testGen
+main = runTestTT $ TestList [testGen, testKill]
 
 {- Helper functions -}
-parseDisplayGen input = displayXList . S.toList . gen . readInstruction $ input
+parseDisplayGen = displayXList . S.toList . gen . readInstruction
+genStringTest result input = TestCase $ assertEqual input result (parseDisplayGen input)
 
+parseDisplayKill = displayXList . S.toList . kill . readInstruction
+killStringTest result input = TestCase $ assertEqual input result (parseDisplayKill input)
 
-{- Gen tests -}
-stringStringTest result input = TestCase $ assertEqual input result (parseDisplayGen input)
-
-testGen = TestList [stringStringTest "(var)" "(eax <- var)"
-                   ,stringStringTest "(eax)" "(var <- eax)"
-                   ,stringStringTest "()" "(var <- (mem ebp -4))"
-                   ,stringStringTest "(x)" "((mem ebp -4) <- x)"
-                   ,stringStringTest "(arr x)" "((mem arr -4) <- x)"
-                   ,stringStringTest "(abba eqx)" "((mem eqx -4) <- abba)"
-                   ,stringStringTest "(x y)" "(x += y)"
-                   ,stringStringTest "(x y)" "(x -= y)"
-                   ,stringStringTest "(a y)" "(y &= a)"
-                   ,stringStringTest "(eax tx)" "(eax += tx)"
-                   ,stringStringTest "()" ":fun"
-                   ,stringStringTest "()" "(goto :fun)"
-                   ,stringStringTest "()" "(goto :eax)"
-                   ,stringStringTest "(eax ecx edx v)" "(call v)"
-                   ,stringStringTest "(eax ecx edi edx esi f)" "(tail-call f)"
-                   ,stringStringTest "(eax edi esi)" "(return)"
-                   ,stringStringTest "(eax y5)" "(eax += y5)"
-                   ,stringStringTest "(arr)" "(eax <- (print arr))"
-                   ,stringStringTest "()" "(ebp -= esp)"
-                   ,stringStringTest "(edi esi)" "(eax <- (allocate esi edi))"
-                   ,stringStringTest "(edi esi)" "(eax <- (array-error esi edi))"
-                   ,stringStringTest "(edx)" "(eax <- (array-error edx 5))"
+{- gen tests -}
+testGen = TestList [genStringTest "(var)" "(eax <- var)"
+                   ,genStringTest "(eax)" "(var <- eax)"
+                   ,genStringTest "()" "(var <- (mem ebp -4))"
+                   ,genStringTest "(x)" "((mem ebp -4) <- x)"
+                   ,genStringTest "(arr x)" "((mem arr -4) <- x)"
+                   ,genStringTest "(abba eqx)" "((mem eqx -4) <- abba)"
+                   ,genStringTest "(x y)" "(x += y)"
+                   ,genStringTest "(x y)" "(x -= y)"
+                   ,genStringTest "(a y)" "(y &= a)"
+                   ,genStringTest "(eax tx)" "(eax += tx)"
+                   ,genStringTest "()" ":fun"
+                   ,genStringTest "()" "(goto :fun)"
+                   ,genStringTest "()" "(goto :eax)"
+                   ,genStringTest "(eax ecx edx v)" "(call v)"
+                   ,genStringTest "(eax ecx edi edx esi f)" "(tail-call f)"
+                   ,genStringTest "(eax edi esi)" "(return)"
+                   ,genStringTest "(eax y5)" "(eax += y5)"
+                   ,genStringTest "(arr)" "(eax <- (print arr))"
+                   ,genStringTest "()" "(ebp -= esp)"
+                   ,genStringTest "(edi esi)" "(eax <- (allocate esi edi))"
+                   ,genStringTest "(edi esi)" "(eax <- (array-error esi edi))"
+                   ,genStringTest "(edx)" "(eax <- (array-error edx 5))"
+                   ,genStringTest "(eax tx)" "(eax += tx)"
+                   ,genStringTest "(a ecx)" "(a <<= ecx)"
+                   ,genStringTest "(a b)" "(q <- a < b)"
+                   ,genStringTest "(f)" "(cjump f = 0 :a :b)"
                    ]
+
+
+{- kill tests -}
+testKill = TestList [killStringTest "()" ":f"
+                    ,killStringTest "(x2)" "(x2 <- eax)"
+                    ,killStringTest "(eax)" "(eax <- var)"
+                    ,killStringTest "(v)" "(v <- (mem ebp -4))"
+                    ,killStringTest "()" "((mem esi -4) <- x)"
+                    ,killStringTest "(x2)" "(x2 *= x2)"
+                    ,killStringTest "(shmuck)" "(shmuck &= shmuck)"
+                    ,killStringTest "(a)" "(a <<= ecx)"
+                    ,killStringTest "(b)" "(b >>= 4)"
+                    ,killStringTest "(q)" "(q <- a < b)"
+                    ,killStringTest "()" "(goto :fish)"
+                    ,killStringTest "()" "(cjump f = 0 :a :b)"
+                    ,killStringTest "(eax ebx ecx edx)" "(call :f)"
+                    ,killStringTest "()" "(tail-call :g)"
+                    ,killStringTest "()" "(return)"
+                    ,killStringTest "(eax ecx edx)" "(eax <- (print a))"
+                    ,killStringTest "(eax ecx edx)" "(eax <- (allocate a 9))"
+                    ,killStringTest "(eax ecx edx)" "(eax <- (array-error a q))"
+                    ]
+
+
+
+
+
